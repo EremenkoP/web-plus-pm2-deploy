48cb20d6511ef8c2c907ece4da64f8af301b08f4
EremenkoP <Eremenko.p.a@yandex.ru>
2023-03-23 22:28:09 +0300

    1th

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Деплой приложения на сервер с использованием pm2
 
 Стартеркит проекта по автоматизации деплоя фронтенда и бэкенда при помощи pm2 (pm2 deploy)
+
+#### IP адрес: 130.193.48.252
+
+#### Frontend: https://mesto-eremenkop.nomoredomains.work/
+
+#### Backend: https://api.mesto-eremenkop.nomoredomains.work/
---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = 'origin/master',
+  DEPLOY_REPO,
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'mesto-frontend',
+      script: '.src/index.js',
+    },
+  ],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd mesto-frontend && npm i && npm run build',
+    },
+  },
+};
